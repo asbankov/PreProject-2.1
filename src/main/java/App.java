06d07b1c9b709775be680c6c7fc7688f50b3da6d
asbankov <ant.s.bankov@gmail.com>
2022-06-29 23:12:34 +0300

    2.1.1 v 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld firstHelloWorld =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld secondHelloWorld =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat firstCat =
+                (Cat) applicationContext.getBean("cat");
+        Cat secondCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(firstHelloWorld == secondHelloWorld);
+        System.out.println(firstCat == secondCat);
     }
 }
